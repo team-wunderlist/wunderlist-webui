09d02dc4f996776bdf0434cb51aefc2691702a7c
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-20 13:52:08 +0100

    Basic NavBar class implementation

---
+++ NavBar/navbar.js
@@ -0,0 +1,41 @@
+class NavBar {
+    constructor(root) {
+        this.root = root;
+        
+        this.createBar()
+        this.createElemements();
+
+        this.appendElements();
+    }
+
+    createBar() {
+        this.header = document.createElement("header");
+        this.nav = document.createElement("nav");
+
+        this.header.classList.add("nav-bar");
+        this.header.appendChild(this.nav);
+    }
+
+    createElemements() {
+        this.logo = document.createElement("h2");
+        this.logo.textContent = "Wunderlist 2.0";
+
+        this.registerTab = document.createElement("div");
+        this.registerTab.classList.add("register-tab");
+
+        this.signUp = document.createElement("button");
+        this.buttonDevider = document.createElement("span");
+        this.login = document.createElement("button");
+
+        this.registerTab.appendChild(this.signUp);
+        this.registerTab.appendChild(this.buttonDevider);
+        this.registerTab.appendChild(this.login);
+    }
+
+    appendElements() {
+        this.nav.appendChild(this.logo);
+        this.nav.appendChild(this.registerTab);
+
+        this.root.appendChild(this.header);
+    }
+}
