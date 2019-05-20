75fb3f9bac88d42e62d492c0b1219f2af3051434
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-20 21:18:04 +0100

    NavButton class added

---
+++ NavMenu/NavMenu.js
@@ -0,0 +1,31 @@
+class NavMenu {
+    constructor() {
+        
+    }
+}
+
+class NavButton {
+    constructor(root) {
+        this.root = root;
+
+        this.createElements();
+        this.appendToRoot();
+    }
+
+    createElements() {
+        this.button = document.createElement("button");
+        this.button.classList("nav-button");
+
+        this.spans = [];
+        for (let _i = 0; _i < 3; _i++) {
+            let span = document.createElement("span");
+            this.spans.push(span);
+            this.button.appendChild(span);
+        }
+    }
+    appendToRoot() {
+        this.root.appendChild(this.button);
+    }
+}
+
+new NavButton(document.body);
