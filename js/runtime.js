2109b66606ff6ca30a119ef2b47166489240ddf6
Brandon Porter <brendlorend@gmail.com>
2019-05-21 15:33:49 -0700

    MostMVP and some stretch: added support for themes

--- js/runtime.js
+++ js/runtime.js
@@ -1,7 +1,5 @@
 import NavBar from './components/nav/NavBar.js'
 import Footer from './components/Footer/footer.js'
-import Theme from './themeChanger.js'
 
 const navbar=new NavBar(document.querySelector('header'))
 const footer=new Footer(document.querySelector('footer'))
-const theme=new Theme(document.querySelectorAll('link[rel="stylesheet"]')[1], document.querySelector('form'))
