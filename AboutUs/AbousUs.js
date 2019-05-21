dddee6c9b6f654b1151566a1742deb0016b206d6
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-21 12:41:34 +0100

    Add createElements class

--- AboutUs/AbousUs.js
+++ AboutUs/AbousUs.js
@@ -2,5 +2,31 @@ class DeveloperProfile {
     constructor(root, info) {
         this.root = root;
         this.info = info;
+
+        this.elements = createElements();
+    }
+
+    createElements() {
+        let elements = {};
+        elements.profile = document.createElement("div");
+        elements.profile.classList.add("developer");
+
+        elements.pictureContainer = document.createElement("div");
+        elements.pictureContainer.classList.add("profile-picture");
+
+        elements.profilePicture = document.createElement("img");
+
+        elements.description = document.createElement("p");
+
+        elements.nameContainer = document.createElement("div");
+        elements.nameContainer.classList.add("name");
+        elements.nameContainer.appendChild(document.createElement("span"));
+
+        elements.name = document.createElement("h2");
+
+        elements.nameContainer.appendChild(this.name);
+        elements.nameContainer.appendChild(document.createElement("span"));
+
+        return elements;
     }
 }
