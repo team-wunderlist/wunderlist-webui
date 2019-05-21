b9f1b8f13b7dbb9ca095530623a7627357295933
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-21 11:46:45 +0100

    Abort calculating distance to move, too much math involved

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function calculateDistanceToMoveBackground(element) {
+/* function calculateDistanceToMoveBackground(element) {
     var st = window.getComputedStyle(element);
     var tr = st.getPropertyValue("-webkit-transform") ||
          st.getPropertyValue("-moz-transform") ||
@@ -15,7 +15,10 @@ function calculateDistanceToMoveBackground(element) {
     var c = values[2]; // -0.5
     var d = values[3]; // 0.866025
 
-    var angle = Math.round(Math.asin(b) * (180/Math.PI));
-    console.log(angle);
+    var angle = Math.asin(b);
+
+    let distanceToMoveY = window.innerHeight * Math.sin(angle) * Math.cos(angle);
+    console.log(distanceToMoveY);
+    element.style.transform = `translateY(-${distanceToMoveY}px) ` + tr;
 }
-calculateDistanceToMoveBackground(document.querySelector(".background"));
+calculateDistanceToMoveBackground(document.querySelector(".background")); */
