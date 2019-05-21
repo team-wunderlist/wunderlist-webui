47196bba2b723acffff8df7f8f80ff6951f8b930
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-21 19:25:05 +0100

    Add InfoDrawer animation

--- InfoDrawer/InfoDrawer.js
+++ InfoDrawer/InfoDrawer.js
@@ -8,13 +8,14 @@ class InfoDrawer {
         this.populateElements(this.elements, this.content);
 
         this.appendElements(this.elements, this.root);
+
+        this.elements.container.addEventListener("click", this.expandDrawer.bind(this))
     }
 
     createElementTree() {
         let elements = {};
         elements.container = document.createElement("div");
         elements.container.classList.add("info-drawer");
-
         elements.title = document.createElement("h2");
         elements.container.appendChild(elements.title);
 
@@ -37,6 +38,28 @@ class InfoDrawer {
 
     appendElements(elements, root) {
         root.appendChild(elements.container);
+    }
+
+    expandDrawer() {
+        if (this.elements.container.classList.contains("open")){
+            TweenMax.to(this.elements.container,0.3, {
+                borderRadius: "50%",
+                width: "80vw"
+            })
+            TweenMax.to(this.elements.title,0.3, {
+                top: "50%",
+            })
+
+        } else {
+            TweenMax.to(this.elements.title,0.3, {
+                top: "5%",
+            })
+            TweenMax.to(this.elements.container,0.3, {
+                borderRadius: "0%",
+                width: "100vw"
+            })
+        }
+        this.elements.container.classList.toggle("open");
     }
 }
 let appInfoContainer = document.querySelector(".app-info");
