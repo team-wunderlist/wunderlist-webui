fe12839760d0306ca781dc1d1e27057ab79b5f94
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-21 19:03:54 +0100

    Add InfoDrawer styles

--- InfoDrawer/InfoDrawer.js
+++ InfoDrawer/InfoDrawer.js
@@ -2,6 +2,12 @@ class InfoDrawer {
     constructor(root, content) {
         this.root = root;
         this.content = content;
+
+        this.elements = this.createElementTree();
+
+        this.populateElements(this.elements, this.content);
+
+        this.appendElements(this.elements, this.root);
     }
 
     createElementTree() {
@@ -14,18 +20,35 @@ class InfoDrawer {
 
         elements.expandButton = document.createElement("span");
         elements.expandButton.classList.add("expand-button");
+        
         elements.container.appendChild(elements.expandButton);
 
         elements.description = document.createElement("p");
         elements.container.appendChild(elements.description);
+
+        return elements;
     }
 
     populateElements(elements, content) {
         elements.title.textContent = content.title;
         elements.description = content.description;
+        elements.expandButton.textContent = "+";
     }
 
     appendElements(elements, root) {
         root.appendChild(elements.container);
     }
 }
+let appInfoContainer = document.querySelector(".app-info");
+new InfoDrawer(appInfoContainer, {
+    title: "Manage your time",
+    description: "Wunderlist allows you to manage your time effectivly and schedual your day in the way you want"
+})
+new InfoDrawer(appInfoContainer, {
+    title: "Manage your time",
+    description: "Wunderlist allows you to manage your time effectivly and schedual your day in the way you want"
+})
+new InfoDrawer(appInfoContainer, {
+    title: "Manage your time",
+    description: "Wunderlist allows you to manage your time effectivly and schedual your day in the way you want"
+})
