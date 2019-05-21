90c5d12f992d5025ef78e9b22f47ab8dcc9e59f3
Brandon Porter <brendlorend@gmail.com>
2019-05-21 13:49:45 -0700

    MostMVP: Added in theming option, no connections yet as no login cycle

--- js/runtime.js
+++ js/runtime.js
@@ -3,3 +3,7 @@ import Footer from './components/Footer/footer.js'
 
 const navbar=new NavBar(document.querySelector('header'))
 const footer=new Footer(document.querySelector('footer'))
+
+function demoTheme(theme){
+    document.querySelectorAll('link[rel="stylesheet"]')[1].setAttribute('href',`/css/${theme}.css`)
+}
