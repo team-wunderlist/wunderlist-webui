de28376c897e0e278f558a6f0659761da3db9273
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-21 14:18:31 +0100

    Add about us content

--- AboutUs/AbousUs.js
+++ AboutUs/AbousUs.js
@@ -49,4 +49,35 @@ class DeveloperProfile {
     }
 }
 
-new DeveloperProfile(document.querySelector(".developer-info"), {name: "Aaron Thompson", description: "test", pictureURI: "AboutUs/profilesImages/aaron.png"});
+// Developers
+const DeveloperArray = [
+    {
+        name: "Aaron Thompson", 
+        description: "I am a 20 year old developer who has been developing software at home for 6 years. I have joined Lambda to make my dreams a reality and to further my passion.", 
+        pictureURI: "AboutUs/profilesImages/aaron.png"
+    },
+    {
+        name: "Mario Amaya",
+        description: "I am 34 years old, and I have a background in Media Production and Video Editing.  I am currently at Lambda School looking to expand my skill set and peruse a new career.",
+        pictureURI: "AboutUs/profilesImages/mario.jpg"
+    },
+    {
+        name: "Leonel Flores",
+        description: "I'm 26 years old and an aspiring developer/programmer going to Lamnda school to jump into the career ive always wanted.",
+        pictureURI: "AboutUs/profilesImages/leonel.jpg"
+    },
+    {
+        name: "Brandon Porter",
+        description: "I am a 34 year old programmer/web developer joining lambda to get a better degree and, from that, a better career.",
+        pictureURI: "AboutUs/profilesImages/brandon.jpg"
+    },
+    {
+        name: "Christian Pelayo",
+        description: "I'm 23 years old and am really just looking expand my knowledge of software development through Lambda School.",
+        pictureURI: "AboutUs/profilesImages/christian.jpg"
+    },
+]
+let developerInfoContainer = document.querySelector(".developer-info");
+DeveloperArray.forEach(dev => {
+    new DeveloperProfile(developerInfoContainer, dev);
+});
