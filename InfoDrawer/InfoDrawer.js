eb844881d9c00584fcde8a742256d0198562ac78
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-21 18:41:13 +0100

    Add InfoDrawer element methods

--- InfoDrawer/InfoDrawer.js
+++ InfoDrawer/InfoDrawer.js
@@ -3,4 +3,29 @@ class InfoDrawer {
         this.root = root;
         this.content = content;
     }
+
+    createElementTree() {
+        let elements = {};
+        elements.container = document.createElement("div");
+        elements.container.classList.add("info-drawer");
+
+        elements.title = document.createElement("h2");
+        elements.container.appendChild(elements.title);
+
+        elements.expandButton = document.createElement("span");
+        elements.expandButton.classList.add("expand-button");
+        elements.container.appendChild(elements.expandButton);
+
+        elements.description = document.createElement("p");
+        elements.container.appendChild(elements.description);
+    }
+
+    populateElements(elements, content) {
+        elements.title.textContent = content.title;
+        elements.description = content.description;
+    }
+
+    appendElements(elements, root) {
+        root.appendChild(elements.container);
+    }
 }
