ef41becdd1b333ff8ba5497356992b74883ed5fa
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-22 12:02:55 +0100

    DeviceSmartphone class done

--- DeviceDemo/DeviceDemo.js
+++ DeviceDemo/DeviceDemo.js
@@ -40,5 +40,47 @@ class DeviceDesktop {
 }
 
 class DeviceSmartphone {
+    constructor(container) {
+        this.container = container;
 
+        this.elements = this.createDeviceElements();
+
+        appendElements(container, this.elements);
+
+        this.screen = this.elements.screen;
+    }
+
+    createDeviceElements() {
+        elements = [];
+        elements.frame = document.createElement("div");
+        elements.frame.classList.add("desktop-device");
+        
+        elements.screen = document.createElement("div");
+        elements.screen.classList.add("screen");
+        
+        elements.camera = document.createElement("span");
+        elements.camera.classList.add("camera");
+
+        elements.speaker = document.createElement("span");
+        elements.camera.classList.add("speaker");
+
+        elements.homeButton = document.createElement("span");
+        elements.homeButton.classList.add("home-button");
+
+        elements.frame.appendChild(elements.camera);
+        elements.frame.appendChild(elements.speaker);
+        elements.frame.appendChild(elements.screen);
+        elements.frame.appendChild(elements.homeButton);
+        
+
+        return elements
+    }
+
+    appendElements(root, elements) {
+        root.appendChild(elements.frame);
+    }
+
+    populateScreen(element) {
+        this.screen.appendChild(element);
+    }
 }
