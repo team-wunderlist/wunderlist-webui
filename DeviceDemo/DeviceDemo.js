1dc7415fc2d4edb8318db57f8ee6c203339ab349
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-22 12:00:38 +0100

    Device desktop class done

---
+++ DeviceDemo/DeviceDemo.js
@@ -0,0 +1,44 @@
+class DeviceShowcase {
+
+}
+
+class DeviceDesktop {
+    constructor(container) {
+        this.container = container;
+
+        this.elements = this.createDeviceElements();
+
+        appendElements(container, this.elements);
+
+        this.screen = this.elements.screen;
+    }
+
+    createDeviceElements() {
+        elements = [];
+        elements.frame = document.createElement("div");
+        elements.frame.classList.add("desktop-device");
+        
+        elements.screen = document.createElement("div");
+        elements.screen.classList.add("screen");
+        
+        elements.camera = document.createElement("span");
+        elements.camera.classList.add("camera");
+
+        elements.frame.appendChild(elements.camera);
+        elements.frame.appendChild(elements.screen);
+
+        return elements
+    }
+
+    appendElements(root, elements) {
+        root.appendChild(elements.frame);
+    }
+
+    populateScreen(element) {
+        this.screen.appendChild(element);
+    }
+}
+
+class DeviceSmartphone {
+
+}
