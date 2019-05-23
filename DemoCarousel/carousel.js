d155f121b0961288ada673e65fdbe85679ca787e
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-24 00:37:46 +0100

    Calculate center transform

--- DemoCarousel/carousel.js
+++ DemoCarousel/carousel.js
@@ -1,3 +1,17 @@
 class Carousel {
-    
+    constructor(container, items) {
+        this.container = container;
+        this.items = items;
+        this.setupItems();
+    }
+
+    setupItems() {
+        let middleScreen = window.innerWidth
+        this.items.forEach((item) => {
+            var rect = item.getBoundingClientRect();
+            item.dataset.centerTransform = `translateX(-${rect.left}px) translateX(50vw) translateX(-50%)`;
+        })
+    }
 }
+
+new Carousel(document.querySelector(".features"), Array.from(document.querySelectorAll(".feature-info")));
