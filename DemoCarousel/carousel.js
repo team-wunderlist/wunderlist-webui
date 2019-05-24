54ec7c96ef000060e52def1e23f948be4aeae80f
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-24 01:16:31 +0100

    Carousel saved for a later date

--- DemoCarousel/carousel.js
+++ DemoCarousel/carousel.js
@@ -6,12 +6,27 @@ class Carousel {
     }
 
     setupItems() {
-        let middleScreen = window.innerWidth
-        this.items.forEach((item) => {
+        let middleScreen = window.innerWidth;
+        let middleItem = Math.floor(this.items.length / 2);
+        let offsetTransforms = this.calculateTransforms(middleItem);
+        console.log(offsetTransforms);
+        this.items.forEach((item, index) => {
             var rect = item.getBoundingClientRect();
-            item.dataset.centerTransform = `translateX(-${rect.left}px) translateX(50vw) translateX(-50%)`;
+            var centerTransform = ` translateX(-${90 * index}vw) translateX(50vw) translateX(-50%)`
+            item.dataset.centerTransform = centerTransform;
+
+            item.style.transform = centerTransform + offsetTransforms[index];
         })
+
+        
+    }
+
+    calculateTransforms(target){
+        let transforms = this.items.map((item, index) => {
+            return `translateX(${(index - target) * 80}vw)`;
+        })
+        return transforms;
     }
 }
 
-new Carousel(document.querySelector(".features"), Array.from(document.querySelectorAll(".feature-info")));
+/* new Carousel(document.querySelector(".features"), Array.from(document.querySelectorAll(".feature-info"))); */
